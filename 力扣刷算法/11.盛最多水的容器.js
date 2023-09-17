c455786913ad2fd29272f6cca9ff28c1d30b1497
继宝 <87047444+ChickenTreasure@users.noreply.github.com>
2023-09-17 12:36:48 +0800

    feat: 11.盛最多水的容器.js

---
+++ 力扣刷算法/11.盛最多水的容器.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
+    while (left < right) {
+        let currentArea = Math.min(height[left], height[right]) * (right - left);
+        maxArea = Math.max(maxArea, currentArea);
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return maxArea;
+};
