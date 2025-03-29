2bc912d4345dc8f07f50113930d37a371ecd6910
ChickenTreasure <2215090293@qq.com>
2025-03-29 18:57:50 +0800

    --story=0 注释添加

--- 力扣刷算法/1.两数之和.js
+++ 力扣刷算法/1.两数之和.js
@@ -21,9 +21,9 @@ var twoSum = function(nums, target) {
     let map = new Map();
     let n = nums.length;
     let ans = new Array(2);
-    for (let i = 0; i < n; i++) {
-        if (map.has(target - nums[i])) {
-            ans[0] = map.get(target - nums[i]);
+    for(let i = 0;i<n;i++) {
+        if(map.has(target-nums[i])){
+            ans[0] = map.get(target-nums[i]);
             ans[1] = i;
         } else {
             map.set(nums[i], i);
